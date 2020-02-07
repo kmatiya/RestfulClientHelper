d0844e2107840ae11f5d577ec3ae15782b659dfa
Kondwani Matiya <kmatiya@nitel.mw>
2020-02-07 11:33:44 +0200

    Add Comment on Rest Parameter to add to requests

--- RestfulClientHelper/ClientRequests/RestModels/RestParameter.cs
+++ RestfulClientHelper/ClientRequests/RestModels/RestParameter.cs
@@ -2,6 +2,9 @@
 
 namespace RestfulClientHelper.ClientRequests.RestModels
 {
+    /*
+        Class with allow a client to set name and value parameters that may be set for a request 
+    */
     public class RestParameter
     {
         public string name { set; get; }
